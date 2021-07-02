157109710d31a0af690da1056367c91d434f3d95
JMGK <jmgk77@gmail.com>
2021-07-02 13:04:22 -0300

    ifdef debug

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,85 @@ WiFiManager wm;
 ESP8266WebServer server;
 ESP8266HTTPUpdateServer httpUpdater;
 
+#define DEBUG 1
+
+/*
+██╗   ██╗████████╗██╗██╗
+██║   ██║╚══██╔══╝██║██║
+██║   ██║   ██║   ██║██║
+██║   ██║   ██║   ██║██║
+╚██████╔╝   ██║   ██║███████╗
+ ╚═════╝    ╚═╝   ╚═╝╚══════╝
+*/
+
+void info()
+{
+#ifdef DEBUG
+  Serial.println("ESP8266 INFO");
+  Serial.printf("\tESP.getResetReason()\t%s\n", ESP.getResetReason().c_str());
+  Serial.printf("\tESP.getFreeHeap()\t%d\n", ESP.getFreeHeap());
+  Serial.printf("\tESP.getHeapFragmentation()\t%d\n", ESP.getHeapFragmentation());
+  Serial.printf("\tESP.getMaxFreeBlockSize()\t%d\n", ESP.getMaxFreeBlockSize());
+  Serial.printf("\tESP.getChipId()\t%d\n", ESP.getChipId());
+  Serial.printf("\tESP.getCoreVersion()\t%s\n", ESP.getCoreVersion().c_str());
+  Serial.printf("\tESP.getSdkVersion()\t%s\n", ESP.getSdkVersion());
+  Serial.printf("\tESP.getCpuFreqMHz()\t%d\n", ESP.getCpuFreqMHz());
+  Serial.printf("\tESP.getSketchSize()\t%d\n", ESP.getSketchSize());
+  Serial.printf("\tESP.getFreeSketchSpace()\t%d\n", ESP.getFreeSketchSpace());
+  Serial.printf("\tESP.getSketchMD5()\t%s\n", ESP.getSketchMD5().c_str());
+  Serial.printf("\tESP.getFlashChipId()\t%d\n", ESP.getFlashChipId());
+  Serial.printf("\tESP.getFlashChipSize()\t%d\n", ESP.getFlashChipSize());
+  Serial.printf("\tESP.getFlashChipRealSize()\t%d\n", ESP.getFlashChipRealSize());
+  Serial.printf("\tESP.getFlashChipSpeed()\t%u\n", (unsigned int)ESP.getFlashChipSpeed);
+  Serial.printf("\tESP.getCycleCount()\t%d\n", ESP.getCycleCount());
+  Serial.printf("\tESP.random()\t%d\n", ESP.random());
+  Serial.printf("\tESP.checkFlashCRC()\t%s\n", ESP.checkFlashCRC() ? "OK" : "NOK");
+  Serial.printf("\tESP.getVcc()\t%d\n", ESP.getVcc());
+#endif
+}
+
+/*
+██╗    ██╗██╗    ██╗██╗    ██╗    ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗ ███████╗
+██║    ██║██║    ██║██║    ██║    ██║  ██║██╔══██╗████╗  ██║██╔══██╗██║     ██╔════╝██╔══██╗██╔════╝
+██║ █╗ ██║██║ █╗ ██║██║ █╗ ██║    ███████║███████║██╔██╗ ██║██║  ██║██║     █████╗  ██████╔╝███████╗
+██║███╗██║██║███╗██║██║███╗██║    ██╔══██║██╔══██║██║╚██╗██║██║  ██║██║     ██╔══╝  ██╔══██╗╚════██║
+╚███╔███╔╝╚███╔███╔╝╚███╔███╔╝    ██║  ██║██║  ██║██║ ╚████║██████╔╝███████╗███████╗██║  ██║███████║
+ ╚══╝╚══╝  ╚══╝╚══╝  ╚══╝╚══╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝
+*/
+
+void handle_reset()
+{
+#ifdef DEBUG
+  Serial.println("Reseting...");
+#endif
+  server.send(200, "text/plain", "Reseting...");
+  wm.resetSettings();
+  ESP.restart();
+}
+
+void handle_root()
+{
+#ifdef DEBUG
+  Serial.println("Main...");
+#endif
+  server.send(200, "text/plain", "Main...");
+}
+
+void handle_404()
+{
+#ifdef DEBUG
+  Serial.println("Not found...");
+#endif
+  server.send(200, "text/plain", "404...");
+}
+
+void install_www_handlers()
+{
+  server.on("/reset", handle_reset);
+  server.on("/", handle_root);
+  server.onNotFound(handle_404);
+}
+
 /*
 ███████╗███████╗████████╗██╗   ██╗██████╗
 ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
@@ -25,35 +104,36 @@ void setup()
   Serial.begin(115200);
   Serial.println("");
   Serial.println("WIFIIR - Starting");
+
+  info();
+
+#ifdef DEBUG
   Serial.setDebugOutput(true);
-  delay(3000);
+  wm.setDebugOutput(true);
+#else
+  Serial.setDebugOutput(false);
+  wm.setDebugOutput(false);
+#endif
 
   if (!wm.autoConnect("WIFIIR"))
   {
+#ifdef DEBUG
     Serial.println("Failed to connect");
+#endif
     ESP.restart();
     delay(1000);
   }
   else
   {
+#ifdef DEBUG
     Serial.println("Connected...");
+#endif
   }
 
   MDNS.begin("WIFIIR");
   httpUpdater.setup(&server);
 
-  server.on("/reset", []()
-            {
-              Serial.println("Reseting...");
-              server.send(200, "text/plain", "Reseting...");
-              wm.resetSettings();
-              ESP.restart();
-            });
-  server.on("/", []()
-            {
-              Serial.println("Main...");
-              server.send(200, "text/plain", "Main...");
-            });
+  install_www_handlers();
 
   server.begin();
   MDNS.addService("http", "tcp", 80);
