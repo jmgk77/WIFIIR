5f5a7b54f6a7f1646de1bdf15288f87e6cfe2e77
JMGK <jmgk77@gmail.com>
2021-07-13 17:52:00 -0300

    ir recv

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,17 @@
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266mDNS.h>
 
+#include <IRrecv.h>
+
+//irRecv vars
+const uint16_t kRecvPin = D7;
+const uint16_t kCaptureBufferSize = 1024;
+const uint8_t kTimeout = 50;
+IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, false);
+bool irin_enable = false;
+
+decode_results results;
+
 WiFiManager wm;
 ESP8266WebServer server;
 ESP8266HTTPUpdateServer httpUpdater;
@@ -105,8 +116,20 @@ void handle_add()
     button = server.arg("button");
   }
   char *r = (char *)malloc(2048);
-  //###warning if 'button' exists
-  strcpy(r, "<br><br><br>\
+  r[0] = 0;
+  if (server.hasArg("used"))
+  {
+    strcat(r, "<div class='card error'>Nome do botão já em uso</div>");
+  }
+  if (server.hasArg("empty"))
+  {
+    strcat(r, "<div class='card error'>Preencha o nome do botão</div>");
+  }
+  if (server.hasArg("nocode"))
+  {
+    strcat(r, "<div class='card error'>Erro na leitura do botão</div>");
+  }
+  strcat(r, "<br><br><br>\
 <form action='/save' method='get'>\
 <p>1. Entre o nome do botão que você quer adicionar</p>\
 <p>2. Espere a luz do WIFIIR começar a piscar</p>\
@@ -132,6 +155,7 @@ void handle_save()
   {
     button = server.arg("button");
   }
+  //###error if 'button' exists
   if ((decoding_onoff) || (button.isEmpty()))
   {
 #ifdef DEBUG
@@ -154,6 +178,14 @@ void handle_save()
       strcat(r, "?button=");
       strcat(r, button.c_str());
     }
+    else
+    {
+      strcat(r, "?empty=1");
+    }
+    if (decoding_onoff)
+    {
+      strcat(r, "&nocode=1");
+    }
     strcat(r, "'</script>");
     send_html(r);
     free(r);
@@ -161,6 +193,7 @@ void handle_save()
   else
   {
     //###save code->button
+    send_html("<div class='card warning'>Botão Salvo!</div>");
   }
 }
 
@@ -271,7 +304,360 @@ void loop()
       led_status = (led_status == HIGH) ? LOW : HIGH;
       digitalWrite(LED_PIN, led_status);
     }
+    //enable
+    if (!irin_enable)
+    {
+      irrecv.enableIRIn(); // Start up the IR receiver.
+      irin_enable = true;
+    }
+
     //decode
-    //###decode
+    if (irrecv.decode(&results))
+    {
+      //salva codigo
+      decode_type_t protocol = results.decode_type;
+      uint16_t size = results.bits;
+      uint64_t value = results.value;
+      //###salva
+
+      //irsend.send(protocol, results.value, size);
+
+#ifdef DEBUG
+      String s;
+      switch (protocol)
+      {
+      case UNKNOWN:
+        s = "UNKNOWN";
+        break;
+      case UNUSED:
+        s = "UNUSED";
+        break;
+      case RC5:
+        s = "RC5";
+        break;
+      case RC6:
+        s = "RC6";
+        break;
+      case NEC:
+        s = "NEC";
+        break;
+      case SONY:
+        s = "SONY";
+        break;
+      case PANASONIC:
+        s = "PANASONIC";
+        break;
+      case JVC:
+        s = "JVC";
+        break;
+      case SAMSUNG:
+        s = "SAMSUNG";
+        break;
+      case WHYNTER:
+        s = "WHYNTER";
+        break;
+      case AIWA_RC_T501:
+        s = "AIWA_RC_T501";
+        break;
+      case LG:
+        s = "LG";
+        break;
+      case SANYO:
+        s = "SANYO";
+        break;
+      case MITSUBISHI:
+        s = "MITSUBISHI";
+        break;
+      case DISH:
+        s = "DISH";
+        break;
+      case SHARP:
+        s = "SHARP";
+        break;
+      case COOLIX:
+        s = "COOLIX";
+        break;
+      case DAIKIN:
+        s = "DAIKIN";
+        break;
+      case DENON:
+        s = "DENON";
+        break;
+      case KELVINATOR:
+        s = "KELVINATOR";
+        break;
+      case SHERWOOD:
+        s = "SHERWOOD";
+        break;
+      case MITSUBISHI_AC:
+        s = "MITSUBISHI_AC";
+        break;
+      case RCMM:
+        s = "RCMM";
+        break;
+      case SANYO_LC7461:
+        s = "SANYO_LC7461";
+        break;
+      case RC5X:
+        s = "RC5X";
+        break;
+      case GREE:
+        s = "GREE";
+        break;
+      case PRONTO:
+        s = "PRONTO";
+        break;
+      case NEC_LIKE:
+        s = "NEC_LIKE";
+        break;
+      case ARGO:
+        s = "ARGO";
+        break;
+      case TROTEC:
+        s = "TROTEC";
+        break;
+      case NIKAI:
+        s = "NIKAI";
+        break;
+      case RAW:
+        s = "RAW";
+        break;
+      case GLOBALCACHE:
+        s = "GLOBALCACHE";
+        break;
+      case TOSHIBA_AC:
+        s = "TOSHIBA_AC";
+        break;
+      case FUJITSU_AC:
+        s = "FUJITSU_AC";
+        break;
+      case MIDEA:
+        s = "MIDEA";
+        break;
+      case MAGIQUEST:
+        s = "MAGIQUEST";
+        break;
+      case LASERTAG:
+        s = "LASERTAG";
+        break;
+      case CARRIER_AC:
+        s = "CARRIER_AC";
+        break;
+      case HAIER_AC:
+        s = "HAIER_AC";
+        break;
+      case MITSUBISHI2:
+        s = "MITSUBISHI2";
+        break;
+      case HITACHI_AC:
+        s = "HITACHI_AC";
+        break;
+      case HITACHI_AC1:
+        s = "HITACHI_AC1";
+        break;
+      case HITACHI_AC2:
+        s = "HITACHI_AC2";
+        break;
+      case GICABLE:
+        s = "GICABLE";
+        break;
+      case HAIER_AC_YRW02:
+        s = "HAIER_AC_YRW02";
+        break;
+      case WHIRLPOOL_AC:
+        s = "WHIRLPOOL_AC";
+        break;
+      case SAMSUNG_AC:
+        s = "SAMSUNG_AC";
+        break;
+      case LUTRON:
+        s = "LUTRON";
+        break;
+      case ELECTRA_AC:
+        s = "ELECTRA_AC";
+        break;
+      case PANASONIC_AC:
+        s = "PANASONIC_AC";
+        break;
+      case PIONEER:
+        s = "PIONEER";
+        break;
+      case LG2:
+        s = "LG2";
+        break;
+      case MWM:
+        s = "MWM";
+        break;
+      case DAIKIN2:
+        s = "DAIKIN2";
+        break;
+      case VESTEL_AC:
+        s = "VESTEL_AC";
+        break;
+      case TECO:
+        s = "TECO";
+        break;
+      case SAMSUNG36:
+        s = "SAMSUNG36";
+        break;
+      case TCL112AC:
+        s = "TCL112AC";
+        break;
+      case LEGOPF:
+        s = "LEGOPF";
+        break;
+      case MITSUBISHI_HEAVY_88:
+        s = "MITSUBISHI_HEAVY_88";
+        break;
+      case MITSUBISHI_HEAVY_152:
+        s = "MITSUBISHI_HEAVY_152";
+        break;
+      case DAIKIN216:
+        s = "DAIKIN216";
+        break;
+      case SHARP_AC:
+        s = "SHARP_AC";
+        break;
+      case GOODWEATHER:
+        s = "GOODWEATHER";
+        break;
+      case INAX:
+        s = "INAX";
+        break;
+      case DAIKIN160:
+        s = "DAIKIN160";
+        break;
+      case NEOCLIMA:
+        s = "NEOCLIMA";
+        break;
+      case DAIKIN176:
+        s = "DAIKIN176";
+        break;
+      case DAIKIN128:
+        s = "DAIKIN128";
+        break;
+      case AMCOR:
+        s = "AMCOR";
+        break;
+      case DAIKIN152:
+        s = "DAIKIN152";
+        break;
+      case MITSUBISHI136:
+        s = "MITSUBISHI136";
+        break;
+      case MITSUBISHI112:
+        s = "MITSUBISHI112";
+        break;
+      case HITACHI_AC424:
+        s = "HITACHI_AC424";
+        break;
+      case SONY_38K:
+        s = "SONY_38K";
+        break;
+      case EPSON:
+        s = "EPSON";
+        break;
+      case SYMPHONY:
+        s = "SYMPHONY";
+        break;
+      case HITACHI_AC3:
+        s = "HITACHI_AC3";
+        break;
+      case DAIKIN64:
+        s = "DAIKIN64";
+        break;
+      case AIRWELL:
+        s = "AIRWELL";
+        break;
+      case DELONGHI_AC:
+        s = "DELONGHI_AC";
+        break;
+      case DOSHISHA:
+        s = "DOSHISHA";
+        break;
+      case MULTIBRACKETS:
+        s = "MULTIBRACKETS";
+        break;
+      case CARRIER_AC40:
+        s = "CARRIER_AC40";
+        break;
+      case CARRIER_AC64:
+        s = "CARRIER_AC64";
+        break;
+      case HITACHI_AC344:
+        s = "HITACHI_AC344";
+        break;
+      case CORONA_AC:
+        s = "CORONA_AC";
+        break;
+      case MIDEA24:
+        s = "MIDEA24";
+        break;
+      case ZEPEAL:
+        s = "ZEPEAL";
+        break;
+      case SANYO_AC:
+        s = "SANYO_AC";
+        break;
+      case VOLTAS:
+        s = "VOLTAS";
+        break;
+      case METZ:
+        s = "METZ";
+        break;
+      case TRANSCOLD:
+        s = "TRANSCOLD";
+        break;
+      case TECHNIBEL_AC:
+        s = "TECHNIBEL_AC";
+        break;
+      case MIRAGE:
+        s = "MIRAGE";
+        break;
+      case ELITESCREENS:
+        s = "ELITESCREENS";
+        break;
+      case PANASONIC_AC32:
+        s = "PANASONIC_AC32";
+        break;
+      case MILESTAG2:
+        s = "MILESTAG2";
+        break;
+      case ECOCLIM:
+        s = "ECOCLIM";
+        break;
+      case XMP:
+        s = "XMP";
+        break;
+      case TRUMA:
+        s = "TRUMA";
+        break;
+      case HAIER_AC176:
+        s = "HAIER_AC176";
+        break;
+      case TEKNOPOINT:
+        s = "TEKNOPOINT";
+        break;
+      case KELON:
+        s = "KELON";
+        break;
+      default:
+        s = "ERROR!";
+      }
+      Serial.print("PROTOCOL: ");
+      Serial.println(s.c_str());
+      Serial.print("VALUE: ");
+      Serial.println(value);
+      Serial.print("SIZE: ");
+      Serial.println(size);
+#endif
+
+      //captura feita
+      irrecv.disableIRIn();
+      led_status = LOW;
+      digitalWrite(LED_PIN, led_status);
+      irin_enable = false;
+      decoding_onoff = false;
+    }
   }
 }
